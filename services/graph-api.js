6caa3193d619662bef7cd2543c7116f754a0b749
mustafaersoyer <mustafaersoyer@gmail.com>
2022-02-02 13:47:16 +0300

    remove second acc

--- services/graph-api.js
+++ services/graph-api.js
@@ -69,10 +69,10 @@ module.exports = class GraphApi {
     if (response.ok) {
       console.log(`Icebreakers have been set.`);
     } else {
-      console.warn(`Error setting ice breakers`, response.body);
+      console.warn(`Error setting ice breakers`, response.statusText);
     }
 
-    //2
+    /*  //2
 
     url.search = new URLSearchParams({
       access_token: config.pageAccesToken2,
@@ -89,10 +89,10 @@ module.exports = class GraphApi {
     if (response.ok) {
       console.log(`Icebreakers have been set.`);
     } else {
-      console.warn(`Error setting ice breakers`, response.body);
+      console.warn(`Error setting ice breakers`, response.statusText);
     }
 
-    //2
+    //2*/
   }
 
   static async setPageSubscriptions() {
@@ -111,7 +111,7 @@ module.exports = class GraphApi {
     }
 
     //2
-
+    /*
     let url2 = new URL(`${config.apiUrl}/${config.pageId2}/subscribed_apps`);
     url2.search = new URLSearchParams({
       access_token: config.pageAccesToken2,
@@ -126,6 +126,6 @@ module.exports = class GraphApi {
       console.warn(`Error setting page subscriptions`, response.statusText);
     }
 
-    //2
+    //2*/
   }
 };
